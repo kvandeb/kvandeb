46f3bace369b352757963634b535851c13f2a35e
Kevin <info@kevinvandenburg.nl>
2016-11-05 14:28:04 +0100

    Finished Reveal on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import $ from 'jquery'; // jQuery in node-modules/
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset){
+    this.itemsToReveal = els;
+    this.offSetPercentage = offset;
+    this.hideInitally();
+    this.createWaypoints();
+  }
+
+  hideInitally() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each( function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: that.offSetPercentage
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
